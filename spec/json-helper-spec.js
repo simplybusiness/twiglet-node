1808f4d0216ab8db693088020eda292cc1d661f1
Nigel 'Fox' Runnels-Moss <sleepyfox@gmail.com>
2020-06-02 11:12:43 +0000

    Change to single-quotes

--- spec/json-helper-spec.js
+++ spec/json-helper-spec.js
@@ -1,95 +1,95 @@
-const jasmine = require("jasmine")
-const json_helper = require("../json-helper")
+const jasmine = require('jasmine')
+const json_helper = require('../json-helper')
 
-describe("json_helper", () => {
-  it("should retain an object without . in any keys", () => {
+describe('json_helper', () => {
+  it('should retain an object without . in any keys', () => {
     const before = {
-      message: "Out of pets exception",
+      message: 'Out of pets exception',
       service: {
-        name: "petshop"
+        name: 'petshop'
       },
       log: {
-        level: "error"
+        level: 'error'
       },
-      "@timestamp": "2020-05-09T15:13:20.736Z"
+      '@timestamp': '2020-05-09T15:13:20.736Z'
     }
 
     const after = json_helper(before)
     expect(before).toEqual(after)
   })
 
-  it("should convert keys with . into nested objects", () => {
+  it('should convert keys with . into nested objects', () => {
     const before = {
-      message: "Out of pets exception",
-      "service.name": "petshop",
-      "log.level": "error",
-      "@timestamp": "2020-05-09T15:13:20.736Z"
+      message: 'Out of pets exception',
+      'service.name': 'petshop',
+      'log.level': 'error',
+      '@timestamp': '2020-05-09T15:13:20.736Z'
     }
 
     const after = {
-      message: "Out of pets exception",
+      message: 'Out of pets exception',
       service: {
-        name: "petshop"
+        name: 'petshop'
       },
       log: {
-        level: "error"
+        level: 'error'
       },
-      "@timestamp": "2020-05-09T15:13:20.736Z"
+      '@timestamp': '2020-05-09T15:13:20.736Z'
     }
 
     expect(json_helper(before)).toEqual(after)
   })
 
-  it("should group nested objects", () => {
+  it('should group nested objects', () => {
     const before = {
-      message: "Out of pets exception",
-      "service.name": "petshop",
-      "service.id": "ps001",
-      "service.version": "0.9.1",
-      "log.level": "error",
-      "@timestamp": "2020-05-09T15:13:20.736Z"
+      message: 'Out of pets exception',
+      'service.name': 'petshop',
+      'service.id': 'ps001',
+      'service.version': '0.9.1',
+      'log.level': 'error',
+      '@timestamp': '2020-05-09T15:13:20.736Z'
     }
 
     const after = {
-      message: "Out of pets exception",
+      message: 'Out of pets exception',
       service: {
-        id: "ps001",
-        name: "petshop",
-        version: "0.9.1"
+        id: 'ps001',
+        name: 'petshop',
+        version: '0.9.1'
       },
       log: {
-        level: "error"
+        level: 'error'
       },
-      "@timestamp": "2020-05-09T15:13:20.736Z"
+      '@timestamp': '2020-05-09T15:13:20.736Z'
     }
 
     expect(json_helper(before)).toEqual(after)
   })
 
-  it("should cope with more than two levels", () => {
+  it('should cope with more than two levels', () => {
     const before = {
-      message: "Escaped pet situation",
-      "service.name": "petshop",
-      "log.level": "debug",
-      "@timestamp": "2020-05-09T15:13:20.736Z",
-      "http.request.method": "get",
-      "http.request.body.bytes": 112,
-      "http.response.bytes": 1564,
-      "http.response.status_code": 200
+      message: 'Escaped pet situation',
+      'service.name': 'petshop',
+      'log.level': 'debug',
+      '@timestamp': '2020-05-09T15:13:20.736Z',
+      'http.request.method': 'get',
+      'http.request.body.bytes': 112,
+      'http.response.bytes': 1564,
+      'http.response.status_code': 200
     }
 
     const after = {
-      message: "Escaped pet situation",
+      message: 'Escaped pet situation',
       service: {
-        name: "petshop"
+        name: 'petshop'
       },
       log: {
-        level: "debug"
+        level: 'debug'
       },
-      "@timestamp": "2020-05-09T15:13:20.736Z",
+      '@timestamp': '2020-05-09T15:13:20.736Z',
       http: {
         request: {
-          method: "get",
+          method: 'get',
           body: {
             bytes: 112
           }
